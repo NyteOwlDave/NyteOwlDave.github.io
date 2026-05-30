3e726a9de16656731dd6462e1c19a0442e081297
NyteOwlDave <nyteowldave@gmail.com>
2026-05-30 01:45:03 -0500

    Legacy ~ List Updates and Karlita Tweaks

--- chachi/api/karlita-extender.js
+++ chachi/api/karlita-extender.js
@@ -54,32 +54,72 @@ function karlita_extender( event ) {
             const id = "store-key.list";
             return edit_list( list, id );
         }
-        if ( code === ascii( "H" ) ) {
-            const list = jst( karlita.hotkeys );
-            const id = "karlita-hotkey.list";
-            return edit_list( list, id );
-        }
-        if ( code === ascii( "L" ) ) {
-            const list = jst( karlita.links );
-            const id = "karlita-link.list";
-            return edit_list( list, id );
-        }
         if ( code === ascii( "M" ) ) {
             const list = KarlitaOps.members();
             const id = "karlita-ops-member.list";
             return edit_list( list, id );
         }
+        if ( code === ascii( "H" ) ) {
+            const id = "karlita-hotkey-list.json";
+            return edit_object( ops.hotkeys, id );
+        }
+        if ( code === ascii( "L" ) ) {
+            const id = "karlita-link-list.json";
+            return edit_object( karlita.links, id );
+        }
+    }
+    function edit_ok( o, id ) {
+        if ( "function" !== typeof zed ) {
+            const c = console;
+            c.group( id );
+            if ( Array.isArray( o ) ) {
+                c.table( o );
+            } else {
+                c.log( o );
+            }
+            c.groupEnd();
+            karlita.message( `See the Console for Results` );
+            return ( false );
+        } else {
+            return ( true );
+        }
+    }
+    function edit_object( obj, id ) {
+        if ( edit_ok( obj, id ) ) {
+            const ed = zed( jst( obj ), 0, id );
+            ed . title = ed . id;
+            ed . focus();
+        }
+        return mine( event );
     }
     function edit_list( list, id ) {
-        const ed = zed( list, 0, id );
-        ed . title = ed . id;
-        ed . focus();
+        if ( edit_ok( list, id ) ) {
+            const ed = zed( list, 0, id );
+            ed . title = ed . id;
+            ed . focus();
+        }
         return mine( event );
     }
 };
 
 karlita_extender.client = null;
 
+karlita_extender.hotkeys = {
+  "TAB" : "Insert TAB Character"
+, "ALT+ENTER" : "Run Script"
+, "ALT+Z" : "Full Screen Mode"
+, "ALT+X" : "Exchange Memo"
+, "ALT+R" : "Read Store Entry"
+, "ALT+W" : "Write Store Entry"
+, "ALT+G" : "Edit Global Client Members"
+, "ALT+H" : "Edit Karlita Hot Keys"
+, "ALT+I" : "Edit Gadget ID Values"
+, "ALT+K" : "Edit Store Keys"
+, "ALT+L" : "Edit Karlita Links"
+, "ALT+M" : "Edit Karlita Members"
+, "ALT+SHIFT+DELETE" : "Remove Editor"
+, "ALT+SHIFT+INSERT" : "New Editor"
+};
 
 addEventListener( "keydown", karlita_extender );
 
