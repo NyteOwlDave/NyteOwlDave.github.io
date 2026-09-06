782766dbc0ceb207f5d535820f8fa3a357d919a6
NyteOwlDave <nyteowl64@gmail.com>
2026-09-06 15:30:20 -0500

    Omega ~ Tweaks for Anchor Decals Gem

--- std/api/gems/replace-anchor-decals.js
+++ std/api/gems/replace-anchor-decals.js
@@ -8,18 +8,25 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-anchor_decals = {
-  R : "🔴"
-, G : "🟢"
+led_color_decals = {
+  RD : "🔴"
+, GR : "🟢"
+, YW : "🟡"
+, OR : "🟠"
+, BL : "🔵"
+, MG : "🟣"
+, BR : "🟤"
+, BK : "⚫"
+, WT : "⚪"
 };
 
-function replace_anchor_decals( verified ) {
-    const RED = anchor_decals.R;
-    const GRN = anchor_decals.G;
+function replace_anchor_decals( verified=[], type="A" ) {
+    const RED = led_color_decals.RD;
+    const GRN = led_color_decals.GR;
     if ( "string" === typeof verified ) {
         verified = [ verified ];
     }
-    const m = find_anchors_by_content( verified );
+    const m = find_anchors_by_content( verified, type );
     function replace( be ) {
         let s = be.innerHTML;
         be.innerHTML = s.replace( RED, GRN );
@@ -29,21 +36,40 @@ function replace_anchor_decals( verified ) {
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-function find_anchors_by_content( list ) {
-    const RED = anchor_decals.R;
-    const GRN = anchor_decals.G;
+function find_anchors_by_content( list=[], type="A" ) {
+    const RED = led_color_decals.RD;
+    const GRN = led_color_decals.GR;
     const strlwr =( s )=> ( str( s ).toLowerCase() );
-    const m = all( "A" );
+    const m = all( str( type ) || "A" );
     const match =( be )=> {
         let s = ( be.textContent );
         s = s.replace( RED, ""  );
         s = s.replace( GRN, ""  );
         return list.includes( strlwr( s ) );
     };
-    list = list.map( strlwr );
+    list = arr( list ).map( strlwr );
     return m.filter( match );
 }
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function remove_anchor_decals( list=[], type="A" ) {
+    const LED = led_color_decals;
+    if ( "string" === typeof list ) {
+        verified = [ list ];
+    }
+    const m = find_anchors_by_content( list, type );
+    function replace( be ) {
+        let s = ( be.textContent );
+        LED.forEach(
+            ( color ) => {
+                s = s.replace( color, "" );
+            }
+        );
+        be.textContent = ( s );
+    }
+    m.forEach( replace );
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
