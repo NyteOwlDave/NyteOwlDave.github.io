b57e3998414fe9f43b371148cb22af3c09cf47d6
NyteOwlDave <nyteowl64@gmail.com>
2026-09-12 03:24:15 -0500

    Omega ~ Bluto Gem Tweaks

--- std/api/bluto/gems/acquire.js
+++ std/api/bluto/gems/acquire.js
@@ -7,29 +7,70 @@
 
 */
 
-message =( s )=> ( messages.textContent = ( s ) );
-suggest =( s )=> ( footer_input.value   = ( s ) );
-
 acquire = function( url ) {
+    const ops = acquire;
     try {
-       const se = elx( "SCRIPT" );
-       const boo = doc.body;
-       boo.appendChild( se );
-       se.src = ( url );
-       return ( se );
+        url = str( url );
+        if (! url.includes( ":" ) ) {
+            return ops.psk( url );
+        }
+        ops . remove( url );
+        const se = elx( "SCRIPT" );
+        const boo = doc.body;
+        boo . appendChild( se );
+        se . src = ( url );
+        return ( se );
     } catch ( e ) {
         console.error( e );
         message( e.message );
     }
 };
 
-acquire.psk = function( p, s, k ) {
-    p = ( str( p ) || "http://dave-omega" );
-    s = ( str( s ) || "app/bluto/api" );
-    k = ( str( k ) || "hello.js" );
-    u = [ p, s, k ].join( "/" );
-	return acquire( u );
+; ( ( ops ) => {
+
+const wnd = window;
+
+ops . locate = function( url ) {
+    const m = arr( doc.scripts );
+    const match =( se )=> ( se.src === url );
+    const v = m.filter( match );
+    return ( v[ 0 ] );
 };
+
+ops . remove = function( url ) {
+    const se = ops.locate( url );
+    if ( se ) { re.remove(); }
+    return ( se );
+};
+
+ops . psk = function( p, s, k ) {
+    try {
+        p = str( p );
+        s = str( s );
+        k = str( k );
+        if (! ( p && s ) ) {
+            k = ( p );
+            s = ( "app/bluto/api" );
+            p = ( "http://dave-omega" );
+        }
+        k = ( k || "hello.js" );
+        u = [ p, s, k ].join( "/" );
+        return ops( u );
+    } catch ( e ) {
+        console.error( e );
+        message( e.message );
+    }
+};
+
+if ( "undefined" === typeof wnd.message ) {
+    wnd.message =( s )=> ( messages.textContent = ( s ) );
+}
+
+if ( "undefined" === typeof wnd.suggest ) {
+    wnd.suggest =( s )=> ( footer_input.value   = ( s ) );
+}
+
+} ) ( acquire );
 
 ;
 ; console.log( `Loaded "acquire.js" Gem Module` )
--- std/api/bluto/gems/describe.js
+++ std/api/bluto/gems/describe.js
@@ -6,6 +6,28 @@ ntype =( o )=> ( alert( o.nodeName ) );
 asize =( a )=> ( alert( a.length   ) );
 osize =( o )=> ( alert( o.size     ) );
 
+function hints( o, title ) {
+    let m, t = str( title );
+    if ( o instanceof Object ) {
+        if ( Array.isArray( o ) ) {
+            m = ( o );
+        } else {
+            m = Object.keys( o || [] ).sort();
+        }
+        t = ( t || "Members" );
+    } else {
+        m = [];
+        m . push( String( o ) );
+        t = ( t || "Value" );
+    }
+    m . unshift( `\n[ ${t} ]\n` );
+    alert( m.join( "\n" ) );
+};
+
+describe = {
+  type, ntype, asize, osize, hints
+};
+
 ;
 ; console.log( `Loaded "describe.js" Gem Module` )
 ;
