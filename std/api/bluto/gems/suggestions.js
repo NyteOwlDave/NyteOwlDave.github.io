9c9d1ba47924bb7918b218fe82dccd8f314693b8
NyteOwlDave <nyteowl64@gmail.com>
2026-09-12 03:44:47 -0500

    Omega ~ Same as Last

---
+++ std/api/bluto/gems/suggestions.js
@@ -0,0 +1,25 @@
+
+/*
+
+    suggestions.js
+    Bluto Gems
+
+*/
+
+
+suggestions = [
+  "install('hud')"
+, "install.inspect()"
+, "acquire()"
+, "hints(install)"
+, "hints(message)"
+, "hints(describe)"
+, "hints(seeker('z'))"
+, "hints(suggestions,'Suggestions')"
+];
+
+
+;
+; console.log( `Loaded "suggestions.js" Gem Module` )
+;
+
